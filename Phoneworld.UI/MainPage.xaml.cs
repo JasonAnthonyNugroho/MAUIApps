ecdbbc4193d523416c642f6a14019bab8046f18e
itsHikariii <hasonanthonynugroho88@gmail.com>
2024-09-13 15:27:47 +0700

    Dial the Phone Number

--- Phoneworld.UI/MainPage.xaml.cs
+++ Phoneworld.UI/MainPage.xaml.cs
@@ -30,12 +30,25 @@
         async void OnCall(object sender, System.EventArgs e)
         {
             if (await this.DisplayAlert(
-                "Dial a Number",
-                "Would you like to call " + translatedNumber + "?",
-                "Yes",
-                "No"))
+        "Dial a Number",
+        "Would you like to call " + translatedNumber + "?",
+        "Yes",
+        "No"))
             {
-                // TODO: dial the phone
+                try
+                {
+                    if (PhoneDialer.Default.IsSupported)
+                        PhoneDialer.Default.Open(translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+                }
+                catch (Exception)
+                {
+                    // Other error has occurred.
+                    await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+                }
             }
         }
 
